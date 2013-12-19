ca03d4fa787d3f9304b8dd1f21be88831e65e905
graceact1989 <graceact1989@gmail.com>
2013-12-19 14:06:18 +0800

    update

---
+++ src/com/grace/test/TestAdd.java
@@ -0,0 +1,7 @@
+package com.grace.test;
+
+public class TestAdd {
+	public static void main(String[] args) {
+		System.out.println("Add file.");
+	}
+}
--- src/com/grace/test/TestSys.java
+++ src/com/grace/test/TestSys.java
@@ -3,6 +3,7 @@ package com.grace.test;
 public class TestSys {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("测试Update");
 	}
 
 }
